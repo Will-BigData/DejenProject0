f12839ceab2393c3dc311f0270773c6cfa77e782
dejtes <dejenaref@gmail.com>
2024-10-07 16:16:19 -0400

    app.py updsted

--- AirCodesApp/AirCode/app.py
+++ AirCodesApp/AirCode/app.py
@@ -12,6 +12,36 @@ class AirportApp:
         print("3. Search Airport by Code")
         print("4. Search Airport by Name")
         print("5. Save and Exit")
+    
+    def run(self):
+        while True:
+            self.display_menu()
+            choice = input("Enter your choice")
+            if choice == "1":
+                self.add_airport()
+            elif choice == "2":
+                self.view_airports()
+            elif choice == "3":
+                self.search_airport_by_code()
+            elif choice == "4":
+                self.search_airport_by_name()
+            elif choice == "5":
+                self.save_and_exit()
+            else:
+                print("Invalid choice")
+    def add_airport(self):
+        code = input("Enter the airport code: ").strip().upper()
+        name = input("Enter the airport name: ").strip()
+
+        if code == "" and name == "":
+            print("Airport code and name cannot be empty")
+        else:
+            result = self.airport_manager.add_airport(code, name)
+            if result:
+                print("Airport added successfully")
+                self.file_manager.save_to_file(self.airport_manager.airports)
+            else:
+                print("Airport already exists")
 
 if __name__ == "__main__":
     app = AirportApp()
