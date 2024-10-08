119b6168c5a73f7f94b623357968778eb21f7f75
dejtes <dejenaref@gmail.com>
2024-10-08 05:09:29 -0400

    add and view airport methods added

--- AirCodesApp/AirCode/airport_manager.py
+++ AirCodesApp/AirCode/airport_manager.py
@@ -10,5 +10,26 @@ class AirportManager:
             if airport["Airport Code"] == code:
                 return True
         return False
-      
+    
+    # add a new airport to the list
+    def add_airport(self, code, name):
+        if self.airport_exists(code):
+            return False, f"Airport with code {code} already exists."
+        else:
+            self.airports.append({"Airport Code": code, "Airport Name": name})
+            return True, f"Airport '{name}' with code '{code}' added successfully"
+        
+    def view_airports(self):
+        if not self.airports:
+            print("No airports found")
+        else:
+            return self.airports
+    
+    def view_airport_by_code(self, code):
+        for airport in self.airports:
+            if airport["Airport Code"] == code:
+                return airport
+        return f"No airport found with code {code}"
+
+
 
