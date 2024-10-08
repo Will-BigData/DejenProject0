7b357c19e370cc2ed55565ed8ae98a942d85f34d
dejtes <dejenaref@gmail.com>
2024-10-08 05:45:14 -0400

    app.py updated

--- AirCodesApp/AirCode/app.py
+++ AirCodesApp/AirCode/app.py
@@ -4,7 +4,12 @@ from airport_manager import AirportManager
 
 class AirportApp:
     def __init__(self):
-        self.airports = []
+        # self.airports = []
+        self.file_manager = FileManager()
+        airports_data = self.file_manager.load_from_file()
+        self.airport_manager = AirportManager(airports_data)
+
+
     def display_menu(self):
         print(" Welcome to Airport Codes Management System Application")
         print("1. Add Airport Code")
@@ -17,18 +22,21 @@ class AirportApp:
         while True:
             self.display_menu()
             choice = input("Enter your choice")
-            if choice == "1":
-                self.add_airport()
-            elif choice == "2":
-                self.view_airports()
-            elif choice == "3":
-                self.search_airport_by_code()
-            elif choice == "4":
-                self.search_airport_by_name()
-            elif choice == "5":
-                self.save_and_exit()
+            if self.validate_choice(choice, ["1", "2", "3", "4", "5"]):
+                if choice == "1":
+                    self.add_airport()
+                elif choice == "2":
+                    self.view_airports()
+                elif choice == "3":
+                    self.search_airport_by_code()
+                elif choice == "4":
+                    self.search_airport_by_name()
+                elif choice == "5":
+                    self.save_and_exit()
+                    break
             else:
-                print("Invalid choice")
+                print("Invalid choice. Please enter a number from 1 to 5. ")
+
     def add_airport(self):
         code = input("Enter the airport code: ").strip().upper()
         name = input("Enter the airport name: ").strip()
@@ -42,6 +50,30 @@ class AirportApp:
                 self.file_manager.save_to_file(self.airport_manager.airports)
             else:
                 print("Airport already exists")
+    
+    def view_airports(self):
+        airports = self.airport_manager.view_airports()
+
+        if isinstance(airports, str):
+            print(airports)
+        else:
+            count= 1
+            for airport in airports:
+                print(f"{count}. Code: {airport['Airport Code']}, Name: {airport['Airport Name']}")
+                count += 1
+
+
+
+
+
+
+    def validate_choice(self, choice, valid_choices):
+        if choice in valid_choices:
+            return True
+        else:
+            print("Invalid choice")
+            return False
+
 
 if __name__ == "__main__":
     app = AirportApp()
